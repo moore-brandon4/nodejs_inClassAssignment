33b67ff0cd91d9c7b0f335f5eb07d55083b74eee
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-11-04 18:28:31 +0000

    part 3 halfway

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var bodyParser = require ('body-parser');
+
 var app = express();
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded( {extended : true}))
+
+var tasks=['wake up', 'eat breakfast'];
+var completed=[];
+
 
 app.get('/', function (request, response){
-    response.send('Hello World!');
+    response.render('index', {tasks : tasks, completed : completed});
+    //response.send('Hello World!');
+});
+
+app.post('/addToDo', function (request,response){
+    response.send('hello');
+});
+
+app.post('/removeToDo', function (request,response){
+    response.send('world');
 });
 
 app.listen(3000, function(){
