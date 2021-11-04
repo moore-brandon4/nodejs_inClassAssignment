02e5e48904626052b102c741ca695dac5b8d778a
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-11-04 21:22:02 +0000

    node part 4 (mondo doesn't update the collection)

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var bodyParser = require ('body-parser');
+// to run code make sure npm install express --save then node index.js
+
+let express = require('express');
+let bodyParser = require ('body-parser');
+let mongoose = require ('mongoose');
+const ToDo=require ('./models/todo.model')
+
 
 var app = express();
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded( {extended : true}))
 
-var tasks=['wake up', 'eat breakfast'];
-var completed=[];
+//connection to mongo
+
+const mongoDB='mongodb+srv://admin:admin@cluster0.o4ixh.mongodb.net/todo?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise=global.Promise;
+let db=mongoose.connection;
+db.on('error', console.error.bind(console,'MongoDB connection error:   '));
+
+let tasks=['wake up', 'eat breakfast','brs'];
+let completed=[];
 
 
 app.get('/', function (request, response){
@@ -15,24 +28,52 @@ app.get('/', function (request, response){
 });
 
 app.post('/addToDo', function (request,response){
-    tasks.push(request.body.newtodo);
+    let newTodo = new ToDo ({
+        item: request.body.newTodo,
+        done: false
+    })
+    newTodo.save(function (err,todo){
+        if(err){
+            console.log(err)
+        }else{
+            response.redirect('/');
+        }
+    });
+    //tasks.push(request.body.newtodo);
     response.redirect('/');
 });
 
 app.post('/removeToDo', function (request,response){
     const remove = request.body.check;
     if(typeof remove === 'string'){
-        tasks.splice( tasks.indexOf(remove),1)
+        ToDo.updateOne({item:remove}, {done:true}, function(err){
+            if(err){
+                console.log(err)
+            }else{
+                response.redirect('/');
+            }
+        })
+        //tasks.splice( tasks.indexOf(remove),1);
+        //completed.push(remove);
     }else if (typeof remove === "object"){
         for (var i=0; i< remove.length; i++){
-            tasks.splice( tasks.indexOf(remove),1)
+            tasks.splice( tasks.indexOf(remove[i]),1);
+            completed.push(remove[i]);
         }
+        response.redirect('/');
     }
-    response.redirect('/');
 });
 
 app.post('/deleteToDo', function (request,response){
-    response.send('world');
+    const deletetasks = request.body.delete;
+    if(typeof deletetasks === 'string'){
+        completed.splice( completed.indexOf(deletetasks),1);
+    }else if (typeof deletetasks === "object"){
+        for (var i=0; i< deletetasks.length; i++){
+            completed.splice( completed.indexOf(deletetasks),1);
+        }
+    }
+    response.redirect('/');
 });
 
 app.listen(3000, function(){
